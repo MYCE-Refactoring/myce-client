c9908241f7b7e8dd4be2e6945f01d9f60599bebb
choeihwa <chldlghk@gmail.com>
2026-02-10 18:57:39 +0900

    fix: modified web socket url

--- src/api/service/chat/ChatWebSocketService.jsx
+++ src/api/service/chat/ChatWebSocketService.jsx
@@ -134,7 +134,7 @@ const connect = async (token, userId) => {
     const getWebSocketURL = () => {
       const baseUrl = import.meta.env.DEV
         ? 'http://localhost:8083/ws/chat'
-        : 'https://myce.cloud/ws/chat';
+        : 'https://api.myce.cloud/ws/chat';
       return `${baseUrl}?ticket=${ticket}`;
     };
 
